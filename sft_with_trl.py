bfc8e0b8025f21863a7393f6318309f1ce355526
Chris Yang <yy4382@outlook.com>
2024-03-29 14:21:54 +0800

    feat: update scripts

--- sft_with_trl.py
+++ sft_with_trl.py
@@ -34,9 +34,9 @@ training_args = TrainingArguments(
     seed=4328,
     logging_steps=5,
     per_device_train_batch_size=4,
-    learning_rate=5e-6,
-    warmup_steps=200,
-    num_train_epochs=4,
+    learning_rate=1e-6,
+    # warmup_steps=200,
+    num_train_epochs=0.5,
     save_total_limit=3,
     save_strategy="steps",
     save_steps=500,
@@ -46,7 +46,7 @@ training_args = TrainingArguments(
 )
 
 sft_trainer = SFTTrainer(
-    "EleutherAI/pythia-410m",
+    "save/trl-sft/log/20240309153857/checkpoint-3500",
     train_dataset=dataset["train"],
     eval_dataset=dataset["test"],
     packing=True,
